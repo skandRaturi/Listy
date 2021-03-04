0f0bd35039ec19c30bd1d481a50a3c0cbdf0b73c
skandRaturi <ishaanraturi.07@gmail.com>
2021-03-04 21:29:11 +0530

    Some changes made to the app some advancements

--- index.php
+++ index.php
@@ -5,24 +5,30 @@ if(isset($_POST['item'])){
     $ammount = $_POST['ammount'];
     $sql = "INSERT INTO `listy`.`shoping_list` (`name`, `ammount`) VALUES ('$name', '$ammount');";
     
-    if($con->query($sql)==true){
-        echo 'successfully inserted';
-    }else{
+    if($con->query($sql)==false){
         echo "Error: $sql <br> $con->error";
     }
     $con->close();
 }
 elseif(isset($_POST['method'])){    
-    if($_POST['method'] == 'put'){
-        $con = mysqli_connect("localhost", "root", "", "listy", "3308") or die(mysqli_error($con));
+    $con = mysqli_connect("localhost", "root", "", "listy", "3308") or die(mysqli_error($con));
+    if($_POST['method'] == 'deleteList'){
         $sql = "TRUNCATE TABLE `listy`.`shoping_list`;";
         if($con->query($sql)==true){
             echo 'successfully deleted';
         }else{
             echo "Error: $sql <br> $con->error";
         }
-        $con->close();
+    }elseif($_POST['method'] == 'deleteItem'){
+        $id = $_POST['id'];
+        $sql = "DELETE FROM `listy`.`shoping_list` WHERE id = '$id';";
+        if($con->query($sql)==true){
+            echo 'successfully deleted';
+        }else{
+            echo "Error: $sql <br> $con->error";
+        }
     }
+    $con->close();    
 }
 ?>
 <!DOCTYPE html>
@@ -35,12 +41,14 @@ elseif(isset($_POST['method'])){
     <title>Listy</title>
     <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/app.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Akaya+Telivigala&family=Satisfy&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="container">
         <h1 class="display-4">L!sty</h1>
-        <div class="container text-center">
+        <div class="container text-center" id="content">
             <div class="row justify-content-md-center">
                 <div class="col-md-6">
                     <form action="index.php" method="post" id="newItem" class="text-aling-center">
@@ -55,30 +63,40 @@ elseif(isset($_POST['method'])){
                             </div>
                         </div>
                         <div class="form-group text-center">
-                            <input type="Submit" value="Add" class="btn btn-sm btn-block btn-outline-success">
+                            <input type="Submit" value="ADD" class="btn btn-sm btn-block btn-outline-success">
                         </div>
                     </form>
                 </div>
                 <br>
             </div>
             <div id="shoppingList" class="">
-                <h2 class="display-5 text-left">Your shopping list</h2>
-                <div class="list-group">
+                <h2 class="display-5 text-left">Your Shopping list</h2>
+                <div class="list-group list-group-flush">
                     <?php 
                         $con = mysqli_connect("localhost", "root", "", "listy", "3308") or die(mysqli_error($con)); 
                     ?>                    
                     <form action="index.php" method="post">
-                        <input type="hidden" name="method" value="put"/>
-                        <button type="submit" class="btn btn-outline-danger btn-block container-fluid" id="deleteButton">Delete</button>
+                        <input type="hidden" name="method" value="deleteList"/>
+                        <button type="submit" class="btn btn-danger btn-block container-fluid" id="deleteButton">DELETE LIST</button>
                     </form>
                     <?php
-                        $sql = "Select * from `shoping_list`;";
+                        $sql = "Select id, name, ammount from `shoping_list`;";
                         $result = mysqli_query($con, $sql);
                         if(!empty($result)){
                             while($row = mysqli_fetch_array($result)){
                     ?>
-                    <li class="list-group-item list-group-item-active items">
+                    <li class="list-group-item list-group-item-active items" type="submit">
                         <span class="float-left"><?php echo $row["name"]; ?></span>
+                        <form action="index.php" method="post" class="float-right hideClass">
+                            <input type="hidden" name="method" value="deleteItem"/> 
+                            <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">                               
+                            <button type="submit" class="deleteItem badge badge-danger btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-low" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg>
+                            </button>
+                        </form>
                         <span class="float-right"><?php echo $row["ammount"]; ?></span>
                     </li>
                     <?php                        
@@ -86,6 +104,7 @@ elseif(isset($_POST['method'])){
                         }
                         $con->close();                       
                     ?>
+                    <li class="list-group-item m-0 p-0"></li>
                 </div>
             </div>
         </div>
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,13 @@
 const listItems = document.querySelectorAll(".items");
 listItems.forEach(listItem => {
     listItem.addEventListener('click', () => {
-        listItem.children[0].classList.toggle('baught');
+        listItem.children[0].classList.add('baught');
+        listItem.children[2].classList.add('baught');
+    });
+    listItem.addEventListener('mouseenter', () => {
+        listItem.children[1].classList.remove('hideClass');
+    });
+    listItem.addEventListener('mouseleave', () => {
+        listItem.children[1].classList.add('hideClass');
     });
 });
